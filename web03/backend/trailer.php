66e0310491d7dd93ba3fdff95471c540670cb8db
yuchi777 <yuchi8943@gmail.com>
2021-09-14 22:59:11 +0800

    製作後台預告片海報管理版面設定

--- web03/backend/trailer.php
+++ web03/backend/trailer.php
@@ -1,1 +1,46 @@
-預告片
+<style>
+    .trailer-header{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .trailer-header .items{
+        width: 24%;
+        background: #bbb;
+        text-align: center;
+        margin: 5px 0 ;
+    }
+
+    .trailer-body{
+        height: 220px;
+        /* padding: 5px; */
+        overflow: auto;
+    }
+</style>
+
+<div class="tab">
+    <div style="height: 320px; width:95%; background:#ccc">
+        <div class="ct" style="border: 1px solid #666;padding:5px">預告片清單</div>
+        <div class="trailer-header">
+            <div class="items">預告片海報</div>
+            <div class="items">預告片片名</div>
+            <div class="items">預告片排序</div>
+            <div class="items">操作</div>
+        </div>
+        <div class="trailer-body">
+
+        </div>
+        <div class="ct trailer-btn">
+            <input type="submit" value="編輯確定">
+            <input type="reset" value="重置">
+        </div>
+
+
+    </div>
+
+    <hr>
+
+    <div style="height: 160px; width:95%; background:#ccc">
+        <div class="ct" style="border: 1px solid #666;padding:5px">新增預告片海報</div>
+    </div>
+</div>
